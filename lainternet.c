5270a5bd502a2a47a76cde36c6150b522b0536a4
lai13 <lai13@protonmail.com>
2018-08-18 00:24:33 -0400

    Adds mail server options to config struct and parsing. Reduces code repitition in parsing.

--- lainternet.c
+++ lainternet.c
@@ -13,7 +13,7 @@ main (int argc, char * argv[])
     config.is_custom_arg = 2;
     config.location = DEFAULT_CONFIG_LOCATION;
     
-    /* Check arguments */
+    /* read arguments */
     static struct option long_options[] =
     {
 	{"config", required_argument, 0, 'c'},
@@ -22,7 +22,8 @@ main (int argc, char * argv[])
 
     int long_index = 0;
     int opt = 0;
-    
+
+    /* loop through command line arguments */
     while ((opt = getopt_long (argc, argv, "c:", long_options,
 			       &long_index)) != -1)
     {
@@ -33,7 +34,7 @@ main (int argc, char * argv[])
 		config.is_custom_arg = 1;
 		break;
 	    default:
-		exit (EXIT_FAILURE);
+		return 1;
 	}
     }
     
@@ -41,21 +42,27 @@ main (int argc, char * argv[])
     FILE * config_file;
     config_file = fopen (config.location, "r");
     int parse_result;
-    if ((parse_result = parse_config_file (&config, config_file)) != 0)
+    parse_result = parse_config_file (&config, config_file);
+
+    fclose (config_file);
+
+    /* there was an error in parsing*/
+    if (parse_result != 0)
     {
 	switch (parse_result)
 	{
 	    case 1:
-		printf ("Error reading configuration file\n");
+		printf ("Error opening configuration file\n");
 		perror (config.location);
 		return 1;
 		break;
 	    case 2:
-		printf ("Configuration file has unrecognized tokens\n");
+		printf ("Configuration file has unrecognized " 
+		       "tokens\n");
 		return 1;
 		break;
 	    default:
-		printf ("An unknown error has occured when" 
+		printf ("An unknown error has occured when " 
 		       "reading the configuration file\n");
 		return 1;
 		break;
@@ -63,9 +70,7 @@ main (int argc, char * argv[])
 	
     }
     
-    fclose (config_file);
-
-    printf ("Email: %s Password: %s\n", config.email, config.password);
+    printf ("Email: %s Password: %s SMTP Mail: %s IMAP Mail: %s\n", config.email, config.password, config.smtp_mail_server, config.imap_mail_server);
     
     return 0;
 }
@@ -77,36 +82,41 @@ parse_config_file (struct lainternet_config * config, FILE * config_file)
     if (config_file == 0)
 	return 1;
     
-    char line[248];
+    char line[512];
 
     /* loop through each line of config file */
     while (fgets (line, sizeof (line), config_file) != 0)
     {
 	/* split line at colon (field:value) */
-	char * token = strtok (line, ":");
-
+	char * token = strtok (line, " ");
+	char ** config_member;
 	if (strcmp (token, "email") == 0)
 	{
-	   /* get second token (value) */
-	   token = strtok (NULL, ":");
-	   /* remove new line characters from string */
-	   token[strcspn (token, "\r\n")] = 0;
-	   config->email = malloc (sizeof (token));
-	   strcpy (config->email, token);
+	   config_member = &(config->email);
 	}
 	else if (strcmp (token, "password") == 0)
 	{
-	   /* get second token (value) */
-	   token = strtok (NULL, ":");
-	   /* remove new line characters from string */
-	   token[strcspn (token, "\r\n")] = 0;
-	   config->password = malloc (sizeof (token));
-	   strcpy (config->password, token);
+	   config_member = &(config->password);
 	}
+	else if (strcmp (token, "smtp-mail-server") == 0)
+	{
+	   config_member = &(config->smtp_mail_server);
+	}
+	else if (strcmp (token, "imap-mail-server") == 0)
+	{
+	    config_member = &(config->imap_mail_server);
+	}
+	/* field is not recognized */
 	else
 	{
 	    return 2;
 	}
+	/* get second token (value) */
+	token = strtok (NULL, " ");
+	/* remove new line characters from string */
+	token[strcspn (token, "\r\n")] = 0;
+	*config_member = malloc (sizeof (token));
+	strcpy (*config_member, token);
     }
     
     return 0;
--- lainternet.h
+++ lainternet.h
@@ -7,6 +7,8 @@ struct lainternet_config
 {
     int  is_custom_arg; /* if has been set by custom arguments */
     char * location; /* location of config file */
+    char * smtp_mail_server; /* URL for SMTP mail server */
+    char * imap_mail_server; /* URL for IMAP mail server */
     char * email; /* email to be used for data transfer */
     char * password; /* password of email */
 };
